55409c251e9ed0bf2812d47b7b9a067f95e3666c
me990928 <me990928@gmail.com>
2024-06-12 02:36:39 +0900

    Initial Commit

---
+++ LogiSync/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  LogiSync
+//
+//  Created by 広瀬友哉 on 2024/06/12.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
