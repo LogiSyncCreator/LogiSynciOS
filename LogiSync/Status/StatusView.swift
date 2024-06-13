26857cf4f042e7856effec6f170721dedcb8ac63
me990928 <me990928@gmail.com>
2024-06-14 02:22:58 +0900

    #31 ステータスビューUI配置

--- LogiSync/Status/StatusView.swift
+++ LogiSync/Status/StatusView.swift
@@ -8,8 +8,44 @@
 import SwiftUI
 
 struct StatusView: View {
+
+    // 対象のユーザーのステータス
+    // ピッカーに合わせて切り替える
+    // VMで変更
+    @State var account = StatusTestData().driverAccount
+    
+    // 受信して変化するユーザーステータス
+    @State var nowStatus = StatusTestData().StatusTagData[0]
+    // 受信して変化するUIステータス
+    // ステータス
+    @State var statusModel = StatusTestData().StatusTagData
+    
+    // UIステータス
+    @State var rollFontColor: Color = Color(.label)
+    @State var thumbWidth: CGFloat = 100
+    @State var thumbStatusIconWidth: CGFloat = 40
+    
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            HStack{
+                SettingButtonUI()
+                Spacer()
+                // ここのピッカーの値変更で表示を切り替えできるようにする
+                RolePicker(selectedRole: $account.selectedRole, fontColor: $rollFontColor)
+            }.padding()
+            Divider()
+            HStack{
+                UserThumbnailUI(width: $thumbWidth, uiImage: $account.uiimage).overlay(alignment: Alignment(horizontal: .trailing, vertical: .bottom)) {
+                    StatusIconUI(symboleColor: $nowStatus.symboleColor, symbole: $nowStatus.symbole, width: $thumbStatusIconWidth)
+                }
+                UserNameUI(name: $account.name, group: $account.group).padding(.leading)
+                Spacer()
+            }.padding()
+            Divider()
+            ProfileUI(profile: $account.profile)
+            StatusList(statusModel: $statusModel, myStatus: $nowStatus)
+        }
     }
 }
 
