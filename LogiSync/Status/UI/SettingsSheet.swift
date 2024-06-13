984a6830b0771d624bda8514aa3693c7aefa05b2
me990928 <me990928@gmail.com>
2024-06-13 23:00:11 +0900

    #1 呼び出しボタン作成

---
+++ LogiSync/Status/UI/SettingsSheet.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsSheet.swift
+//  LogiSync
+//
+//  Created by 広瀬友哉 on 2024/06/13.
+//
+
+import SwiftUI
+
+struct SettingsSheet: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsSheet()
+}
