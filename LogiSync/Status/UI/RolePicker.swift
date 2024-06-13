efccd226e237cb9321938daaabd2044d91ec2dda
me990928 <me990928@gmail.com>
2024-06-13 23:13:52 +0900

    add  file RolePicker

---
+++ LogiSync/Status/UI/RolePicker.swift
@@ -0,0 +1,18 @@
+//
+//  RolePicker.swift
+//  LogiSync
+//
+//  Created by 広瀬友哉 on 2024/06/13.
+//
+
+import SwiftUI
+
+struct RolePicker: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RolePicker()
+}
