827621b154071f854866fc428725902d2860e6bf
me990928 <me990928@gmail.com>
2024-06-14 01:29:24 +0900

    add file StatusList & StatusTagModelTest & StatusTestData

---
+++ LogiSync/TestData/Status/StatusTagModelTest.swift
@@ -0,0 +1,8 @@
+//
+//  StatusTagModelTest.swift
+//  LogiSync
+//
+//  Created by 広瀬友哉 on 2024/06/14.
+//
+
+import Foundation
---
+++ LogiSync/TestData/Status/StatusTestData.swift
@@ -0,0 +1,8 @@
+//
+//  StatusTestData.swift
+//  LogiSync
+//
+//  Created by 広瀬友哉 on 2024/06/14.
+//
+
+import Foundation
