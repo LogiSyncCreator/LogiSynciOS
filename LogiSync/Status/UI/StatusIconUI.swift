fbe14ff1c171a7f9678d6c84b20c03675ba665ca
me990928 <me990928@gmail.com>
2024-06-13 23:49:04 +0900

    #3 ステータスアイコン作成

--- LogiSync/Status/UI/StatusIconUI.swift
+++ LogiSync/Status/UI/StatusIconUI.swift
@@ -7,12 +7,26 @@
 
 import SwiftUI
 
+// ステータスアイコン
 struct StatusIconUI: View {
+    
+    @Binding var symboleColor: Color    // SFシンボルの色
+    @Binding var backColor: Color       // アイコンの背景色
+    @Binding var symbole: String        // SFシンボル
+    @Binding var width: CGFloat         // サイズ
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack{
+            Circle().frame(width: width).foregroundStyle(symboleColor)
+            Image(systemName: symbole).font(.system(size: width - 5)).foregroundColor(backColor)
+        }
     }
 }
 
-#Preview {
-    StatusIconUI()
-}
+//#Preview {
+//    オフライン
+//    StatusIconUI(symboleColor: Color(.systemBackground), backColor: Color(.gray), symbole: "xmark.circle", width: 30)
+//    オンライン
+//    StatusIconUI(symboleColor: Color(.systemBackground), backColor: Color(.green), symbole: "checkmark.circle.fill", width: 30)
+    
+//}
