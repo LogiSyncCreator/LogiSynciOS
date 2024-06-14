7e391be9264e0714c32b154077bb0deb77505fd6
me990928 <me990928@gmail.com>
2024-06-14 13:22:22 +0900

    add file UserLocationSendButton

---
+++ LogiSync/Map/UI/UserLocationSendButtonUI.swift
@@ -0,0 +1,18 @@
+//
+//  UserLocationSendButtonUI.swift
+//  LogiSync
+//
+//  Created by 広瀬友哉 on 2024/06/14.
+//
+
+import SwiftUI
+
+struct UserLocationSendButtonUI: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    UserLocationSendButtonUI()
+}
