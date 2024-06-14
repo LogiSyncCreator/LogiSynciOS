417ee3e33632fb200ae4d2e21133583e09ed5911
me990928 <me990928@gmail.com>
2024-06-14 13:22:45 +0900

    #10 現在地を送信するボタンの作成

--- LogiSync/Map/UI/UserLocationSendButtonUI.swift
+++ LogiSync/Map/UI/UserLocationSendButtonUI.swift
@@ -9,7 +9,15 @@ import SwiftUI
 
 struct UserLocationSendButtonUI: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Button {
+            
+        } label: {
+            HStack{
+                Image(systemName: "mappin.circle")
+                Text("位置情報を送信")
+            }.font(.title3).foregroundStyle(Color(.label)).padding()
+        }.background(Color(.systemGray4), in: RoundedRectangle(cornerRadius: 25.0))
+
     }
 }
 
