cd63ae6e8039829258e726ed88fe273842d3360d
me990928 <me990928@gmail.com>
2024-06-14 13:15:35 +0900

    add file MapView, MapBody

---
+++ LogiSync/Map/MapView.swift
@@ -0,0 +1,18 @@
+//
+//  MapView.swift
+//  LogiSync
+//
+//  Created by 広瀬友哉 on 2024/06/14.
+//
+
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MapView()
+}
