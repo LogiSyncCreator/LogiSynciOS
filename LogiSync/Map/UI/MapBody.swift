74e7bd643ec16abcc8c1b56a7f2493d200c3a7ad
me990928 <me990928@gmail.com>
2024-06-14 13:36:49 +0900

    #11 送信された位置情報のシンボル

--- LogiSync/Map/UI/MapBody.swift
+++ LogiSync/Map/UI/MapBody.swift
@@ -9,9 +9,18 @@ import SwiftUI
 import MapKit
 
 struct MapBody: View {
+    
+    var sendCircleColor: UIColor = UIColor(red: 200, green: 30, blue: 0, alpha: 0.3)
+    
     var body: some View {
         Map(){
             
+            // ここから#11 送信したマップのサークル
+            Marker(coordinate: CLLocationCoordinate2D(latitude: 35.168477, longitude: 136.8857)) {
+                Text("22:30")
+            }
+            MapCircle(center: CLLocationCoordinate2D(latitude: 35.168477, longitude: 136.8857), radius: CLLocationDistance(50)).foregroundStyle(Color(uiColor: sendCircleColor))
+            // #11
         }
     }
 }
