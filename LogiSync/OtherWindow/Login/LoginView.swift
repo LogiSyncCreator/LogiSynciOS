76a2329c1500411d2aa982fdcda34450ce5ce82f
me990928 <me990928@gmail.com>
2024-06-15 15:11:33 +0900

    #24 #26 #28 #29 完了

---
+++ LogiSync/OtherWindow/Login/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  LogiSync
+//
+//  Created by 広瀬友哉 on 2024/06/15.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
