2af2ec8342ac2fe37cc3d2f0c6e4780fd2a26153
me990928 <me990928@gmail.com>
2024-06-15 14:19:17 +0900

    add file CustomTabView

---
+++ LogiSync/OtherWindow/OtherContentView.swift
@@ -0,0 +1,18 @@
+//
+//  OtherContentView.swift
+//  LogiSync
+//
+//  Created by 広瀬友哉 on 2024/06/15.
+//
+
+import SwiftUI
+
+struct OtherContentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OtherContentView()
+}
---
+++ LogiSync/UI/CustomTabView.swift
@@ -0,0 +1,18 @@
+//
+//  CustomTabView.swift
+//  LogiSync
+//
+//  Created by 広瀬友哉 on 2024/06/15.
+//
+
+import SwiftUI
+
+struct CustomTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomTabView()
+}
