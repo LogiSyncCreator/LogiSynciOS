9ae39153f85b414229d1522faa80e304682b5a60
me990928 <me990928@gmail.com>
2024-06-16 14:25:56 +0900

    #21 登録・確認ボタン

--- LogiSync/OtherWindow/Regist/UI/RegistButton.swift
+++ LogiSync/OtherWindow/Regist/UI/RegistButton.swift
@@ -8,8 +8,26 @@
 import SwiftUI
 
 struct RegistButton: View {
+    @State var isAlert = false
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Button(action: {
+            // 送信前の確認アラート
+            isAlert.toggle()
+        }, label: {
+            Text("登録")
+        }).buttonStyle(BorderedButtonStyle())
+            .alert("警告", isPresented: $isAlert) {
+                Button("送信") {
+                    
+                }
+                Button(role: .cancel) {
+                    
+                } label: {
+                    Text("戻る")
+                }
+            } message: {
+                Text("送信してもよろしいですか？")
+            }
     }
 }
 
