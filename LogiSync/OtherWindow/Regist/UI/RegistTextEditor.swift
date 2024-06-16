f07fba74675e81270cf8217e98785caeadbd0aa8
me990928 <me990928@gmail.com>
2024-06-16 14:10:34 +0900

    add files

---
+++ LogiSync/OtherWindow/Regist/UI/RegistTextEditor.swift
@@ -0,0 +1,18 @@
+//
+//  RegistTextEditor.swift
+//  LogiSync
+//
+//  Created by 広瀬友哉 on 2024/06/16.
+//
+
+import SwiftUI
+
+struct RegistTextEditor: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RegistTextEditor()
+}
