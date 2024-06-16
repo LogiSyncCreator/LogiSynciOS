8af5f9a8fde0448914ac51c0848c1d4e12811276
me990928 <me990928@gmail.com>
2024-06-16 15:07:49 +0900

    add file registVew

---
+++ LogiSync/OtherWindow/Regist/RegistView.swift
@@ -0,0 +1,18 @@
+//
+//  RegistView.swift
+//  LogiSync
+//
+//  Created by 広瀬友哉 on 2024/06/16.
+//
+
+import SwiftUI
+
+struct RegistView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RegistView()
+}
