a08a3e6e290af352a04d5e54281e780c319802fc
me990928 <me990928@gmail.com>
2024-06-16 15:08:21 +0900

    update statements

--- LogiSync/OtherWindow/Regist/UI/RegistTextEditor.swift
+++ LogiSync/OtherWindow/Regist/UI/RegistTextEditor.swift
@@ -8,13 +8,13 @@
 import SwiftUI
 
 struct RegistTextEditor: View {
-    @State var text: String
+    @Binding var text: String
     @State var placeholderString: String
     
     @FocusState var isFocus: Bool
     
     var body: some View {
-        TextEditor(text: $text).frame(height: 100).focused($isFocus)
+        TextEditor(text: $text).frame(height: 200).focused($isFocus)
             .foregroundColor(self.text == placeholderString ? .gray : .primary)
             .onTapGesture {
               if self.text == placeholderString {
@@ -29,8 +29,8 @@ struct RegistTextEditor: View {
     }
 }
 
-#Preview {
-    ScrollView{
-        RegistTextEditor(text: "氏名を入力してください", placeholderString: "氏名を入力してください")
-    }.scrollDismissesKeyboard(.immediately)
-}
+//#Preview {
+//    ScrollView{
+//        RegistTextEditor(text: "氏名を入力してください", placeholderString: "氏名を入力してください")
+//    }.scrollDismissesKeyboard(.immediately)
+//}
