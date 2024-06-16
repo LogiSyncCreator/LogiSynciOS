f7e680dc2c03966fa40990df670dcea61830729a
me990928 <me990928@gmail.com>
2024-06-16 14:25:28 +0900

    add file registButton

--- LogiSync/OtherWindow/Regist/UI/RegistButton.swift
+++ LogiSync/OtherWindow/Regist/UI/RegistButton.swift
@@ -1,5 +1,5 @@
 //
-//  RolePicker.swift
+//  RegistButton.swift
 //  LogiSync
 //
 //  Created by 広瀬友哉 on 2024/06/16.
@@ -7,12 +7,12 @@
 
 import SwiftUI
 
-struct RolePicker: View {
+struct RegistButton: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    RolePicker()
+    RegistButton()
 }
