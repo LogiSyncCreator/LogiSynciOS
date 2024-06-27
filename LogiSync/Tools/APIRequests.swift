0d087ecc30445c1f392863e05539fbfb021e4652
me990928 <me990928@gmail.com>
2024-06-28 04:01:09 +0900

    通信用テンプレ作成

--- LogiSync/Tools/APIRequests.swift
+++ LogiSync/Tools/APIRequests.swift
@@ -6,3 +6,123 @@
 //
 
 import Foundation
+
+class APIRequests {
+    let host: String = "192.168.68.82"
+    let port: String = "8080"
+    let httpd: String = "http"
+    
+    
+    func userLogin(id: String, pass: String) async throws -> Data {
+        let postData: [String: Any] = ["username":id, "password":pass]
+        return try await APIRequest(postData: postData, endPoint: "login")
+    }
+    
+    func getStatus(id: String) async throws -> Data {
+        return try await APIRequest(param: id, endPoint: "nowstatus")
+    }
+    
+    /// Description
+    /// - Parameters:
+    ///   - param: http://******/{param}
+    ///   - postData: ["key": value, ...]
+    ///   - endPoint: http://{endpoint}/{param}
+    ///   - method: GET or DELETE
+    func APIRequest(param: String, endPoint: String) async throws {
+        guard let url = URL(string: "\(httpd)://\(host):\(port)/\(endPoint)/\(param)") else {
+            throw URLError(.badURL)
+        }
+        // URLRequestオブジェクトを作成
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        
+        // URLSessionを使用してリクエストを送信
+        let (_, response) = try await URLSession.shared.data(for: request)
+        
+        // レスポンスのステータスコードをチェック
+        if let httpResponse = response as? HTTPURLResponse, !(200...299).contains(httpResponse.statusCode) {
+            throw URLError(.badServerResponse)
+        }
+    }
+    
+    /// Description
+    /// - Parameters:
+    ///   - param: http://******/{param}
+    ///   - postData: ["key": value, ...]
+    ///   - endPoint: http://{endpoint}/{param}
+    ///   - method: POST or DELETE
+    func APIRequest(postData: [String: Any], endPoint: String) async throws {
+        guard let url = URL(string: "\(httpd)://\(host):\(port)/\(endPoint)") else {
+            throw URLError(.badURL)
+        }
+        // URLRequestオブジェクトを作成
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        // POSTデータを設定
+        request.httpBody = try JSONSerialization.data(withJSONObject: postData, options: [])
+        
+        // URLSessionを使用してリクエストを送信
+        let (_, response) = try await URLSession.shared.data(for: request)
+        
+        // レスポンスのステータスコードをチェック
+        if let httpResponse = response as? HTTPURLResponse, !(200...299).contains(httpResponse.statusCode) {
+            throw URLError(.badServerResponse)
+        }
+    }
+    
+    /// Description
+    /// - Parameters:
+    ///   - param: http://******/{param}
+    ///   - postData: ["key": value, ...]
+    ///   - endPoint: http://{endpoint}/{param}
+    ///   - method: GET or DELETE
+    func APIRequest(param: String, endPoint: String) async throws -> Data {
+        guard let url = URL(string: "\(httpd)://\(host):\(port)/\(endPoint)/\(param)") else {
+            throw URLError(.badURL)
+        }
+        // URLRequestオブジェクトを作成
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        
+        // URLSessionを使用してリクエストを送信
+        let (data, response) = try await URLSession.shared.data(for: request)
+        
+        // レスポンスのステータスコードをチェック
+        if let httpResponse = response as? HTTPURLResponse, !(200...299).contains(httpResponse.statusCode) {
+            throw URLError(.badServerResponse)
+        }
+        
+        return data
+    }
+    
+    /// Description
+    /// - Parameters:
+    ///   - param: http://******/{param}
+    ///   - postData: ["key": value, ...]
+    ///   - endPoint: http://{endpoint}/{param}
+    ///   - method: POST or DELETE
+    func APIRequest(postData: [String: Any], endPoint: String) async throws -> Data {
+        guard let url = URL(string: "\(httpd)://\(host):\(port)/\(endPoint)") else {
+            throw URLError(.badURL)
+        }
+        // URLRequestオブジェクトを作成
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        // POSTデータを設定
+        request.httpBody = try JSONSerialization.data(withJSONObject: postData, options: [])
+        
+        // URLSessionを使用してリクエストを送信
+        let (data, response) = try await URLSession.shared.data(for: request)
+        
+        // レスポンスのステータスコードをチェック
+        if let httpResponse = response as? HTTPURLResponse, !(200...299).contains(httpResponse.statusCode) {
+            throw URLError(.badServerResponse)
+        }
+        
+        return data
+    }
+}
