4c173c8e4a72e1a3a20b0f086c99c2a5d73eafb7
me990928 <me990928@gmail.com>
2024-07-05 04:36:37 +0900

    テスト通知クラス

--- LogiSync/Tools/TestLocalNotification.swift
+++ LogiSync/Tools/TestLocalNotification.swift
@@ -6,3 +6,34 @@
 //
 
 import Foundation
+import UserNotifications
+
+final class TestLocalNotification {
+    
+    func createNotificationContent() -> UNNotificationContent {
+        let content = UNMutableNotificationContent()
+        content.title = "テスト用"
+        content.body = "この通知は見えていないはずだよ！"
+        content.sound = UNNotificationSound.default
+        return content
+    }
+    
+    func createTrigger() -> UNNotificationTrigger {
+        // 例えば5秒後に通知を表示するトリガー
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 1, repeats: false)
+        return trigger
+    }
+    
+    func scheduleNotification() {
+        let content = createNotificationContent()
+        let trigger = createTrigger()
+        let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+        
+        UNUserNotificationCenter.current().add(request) { error in
+            if let error = error {
+                print("通知のスケジュールに失敗しました: \(error.localizedDescription)")
+            }
+        }
+    }
+    
+}
