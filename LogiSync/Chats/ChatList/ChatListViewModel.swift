9bc0e2f11e18e4f528c6e1351b6efe9158d841fb
me990928 <me990928@gmail.com>
2024-07-17 00:27:31 +0900

    チャットのセーブとロード

--- LogiSync/Chats/ChatList/ChatListViewModel.swift
+++ LogiSync/Chats/ChatList/ChatListViewModel.swift
@@ -6,7 +6,46 @@
 //
 
 import Foundation
+import Combine
 
 class ChatListViewModel: ObservableObject {
+    @Published var chats: [Chats] = []
+    
+    private let key: String = "chats"
+    
+    private var cancellables = Set<AnyCancellable>()
+    public let saveChat = PassthroughSubject<Void, Never>()
+    public let loadChat = PassthroughSubject<Void, Never>()
+    
+    init(){
+        self.saveChat.sink { () in
+            self.saveChats()
+        }.store(in: &cancellables)
+        
+        self.loadChat.sink { () in
+            self.loadChats()
+        }.store(in: &cancellables)
+        
+    }
+    
+    func saveChats() {
+        do {
+            let data = try JSONEncoder().encode(chats)
+            UserDefaults.standard.setValue(data, forKey: key)
+        } catch {
+            print("chat data save error.")
+        }
+    }
+    
+    func loadChats() {
+        do {
+            let data = UserDefaults.standard.data(forKey: key)
+            if let data = data {
+                self.chats = try JSONDecoder().decode([Chats].self, from: data)
+            }
+        } catch {
+            print("chat data load error.")
+        }
+    }
     
 }
